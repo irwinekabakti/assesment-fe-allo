abd2d1ea29ebb325581030cd5ef0e3010d83456e
irwinekabakti <irwinekabakti@gmail.com>
2026-01-13 11:27:35 +0700

    add:(change-7)

--- src/api/base_api.ts
+++ src/api/base_api.ts
@@ -1,15 +1,18 @@
 import axios, { type AxiosResponse } from "axios";
 import type { Rocket } from '@/types/rocket';
 
+
 const api = axios.create({
-  baseURL: "https://api.spacexdata.com",
+  baseURL: import.meta.env.VITE_BASE_API_URL,
   timeout: 3500,
 });
+
+console.log(import.meta.env.VITE_BASE_API_URL, '<---');
 
 
 export async function getRockets(): Promise<AxiosResponse<Rocket[]>> {
   const response = await api.get("/v4/rockets");
-  console.log(response.data, '<--- from base_api.ts')
+  console.log(response, '<--- from base_api.ts')
   return response;
 }
 
